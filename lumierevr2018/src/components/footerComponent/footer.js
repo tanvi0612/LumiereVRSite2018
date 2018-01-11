a39ff0b89f877e97eb061fe4e48b8391c8192e4d
Tanvi <tanvi@lumierevr.com>
2018-01-11 18:15:36 -0500

    newly created git folder for lumiereVR site

--- lumierevr2018/src/components/footerComponent/footer.js
+++ lumierevr2018/src/components/footerComponent/footer.js
@@ -4,9 +4,27 @@ class Footer extends Component {
   render() {
     return (
       <footer>
+      	<div className= "container">
+  			<div className="pull-left">
+  				<ul>
+	  				<li><a href="/">Join Us</a></li>
+	  				<li><a href="/">Contact Us</a></li>
+	  				<li><a href="/">Terms and Conditions</a></li>
+	  				<li><a href="/">Privacy Policy</a></li>
+  				</ul>
+  				<ul>
+	  				<li><a href="/">Press Kit</a></li>
+	  				<li><a href="/">Blog</a></li>
+  				</ul>
+  			</div>
 
-        Contact/footer components.
-
+  			<div className="pull-right">
+  				<p>Follow Us!</p><br></br>
+  				<img src={require('../../images/footer/twitter.png')}/>
+  				<img src={require('../../images/footer/fb.png')}/>
+  				<img src={require('../../images/footer/yt.png')}/>
+  			</div>
+  		</div>
       </footer>
     );
   }
--- lumierevr2018/src/components/pages/distribute.js
+++ lumierevr2018/src/components/pages/distribute.js
@@ -19,8 +19,8 @@ class Distribute extends Component {
         </h2>
         <div className="case1TheaterWrapperDiv">
           <div className = "case1TheaterLeftDiv">
-            <img src={require('../../images/theaters/Case1TheaterRender.png')}/> 
-            <img src={require('../../images/theaters/Case1TheaterDemo.png')}/>
+            <img className="colImg" src={require('../../images/theaters/Case1TheaterRender.png')}/> 
+            <img className="colImg" src={require('../../images/theaters/Case1TheaterDemo.png')}/>
           </div>
           <div className = "case1TheaterRightDiv">
             <p>
@@ -49,49 +49,68 @@ class Distribute extends Component {
        
         <div className="case1TheaterBottomWrapperDiv">
           <div className="case1TheaterBottomLeftDiv">
-            <img src={require('../../images/theaters/Case1Heatmap.png')}/> 
+            <img className="rowImg" src={require('../../images/theaters/Case1Heatmap.png')}/> 
           </div>
           <div className="case1TheaterBottomRightDiv">
-            <img src={require('../../images/theaters/Case1Graph.png')}/>
+            <img className="rowImg" src={require('../../images/theaters/Case1Graph.png')}/>
           </div> 
         </div>
         <hr color="black" />
+
         <h1>
           Case 2
         </h1>
+
         <h2 align="center"> 
           Festival/Conference Pop-Up VR
         </h2>
+
         <div className="case2TheaterWrapperDiv">
+
           <div className = "case2TheaterLeftDiv">
-            <img src={require('../../images/theaters/Case1TheaterRender.png')}/> 
-            <img src={require('../../images/theaters/Case1TheaterDemo.png')}/>
+            <img className="colImg" src={require('../../images/theaters/Case2TheaterDemo.png')}/> 
+            <img className="colImg" src={require('../../images/theaters/Case2Setup.png')}/>
           </div>
-          <div className = "case1TheaterRightDiv">
+
+          <div className = "case2TheaterRightDiv">
             <p>
-              Partnering with the creators of The Click Effect as well as Zoos, Aquariums, and Museums we use a syncronized system of stand alone head-sets controlled by a master Tablet  
+              As a service to VR/Tech Conferences or Film Festivals, we’ve designed two software options instead of syncronous playback in order to account for crowd mob behaviour as well as optimizing throughput 
+              Costs are usually $6000-$8000 per event
             </p>
             <p>
-              Theaters are usually fairly portable, they come with collapsible walls and ambient light, along with N number of swivel chairs. 
+              Option 1: <br></br>
+              Software pro-programed via tablet to play designated film, user puts on headsets and film starts. User removes headset, film resets to the
+              beginning. 
             </p>
             <p>
-              Headset system running continuously at 15 minutes per interval lasts 7 hours a day +- 30 minutes. 
+              Option 2: <br></br>
+              Headset is pre-programed to play disignated film at a certain time. Audience is lead in early and fitted with headsets that sit in a lobby
+              screen until set time. 
             </p>
             <p>
-              Standard micro-usb charging towers ensure headsets are fully charged at night and ready to run in the morning.
+              For your events contact <a href= "mailto:theaters@lumierevr.com">theaters@lumierevr.com</a> for more details!
             </p>
             <p>
-              On the left is an installment we’ve built for Prairie Fire in Kansas City. 
-            </p>
-            <p>
-              On the bottom is complementary analysis of both performance and behaviour from headtracking heatmaps powered by Retinad.
-            </p>
-            <p>
-              Email <a href = "mailto:theaters@lumierevr.com"> theaters@lumierevr.com </a> to learn how you and your brand can benifit from offline location-based VR distribution systems. 
+              Top Left: VR Fest by Jump Into The Light <br></br>
+              Left: VIP Event for the Canadian Media Fund <br></br> 
+              Bottom: Gold Coast Motor Festival by JP Morgan <br></br>
             </p>
           </div>
         </div>
 
+        <div className="case2TheaterBottomWrapperDiv"> 
+          <div className="case2TheaterBottomLeftDiv ">
+            <img className="rowImg" src={require('../../images/theaters/Case2SitdownParty.png')}/> 
+          </div>
+
+          <div className="case2TheaterBottomMidDiv ">
+            <img className="rowImg" src={require('../../images/theaters/Case2Lounge.png')}/>
+          </div>
+
+          <div className="case2TheaterBottomRightDiv ">
+            <img className="rowImg" src={require('../../images/theaters/Case2Badge.png')}/> 
+          </div>
+        </div>
 
            
       </div>
