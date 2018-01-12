d127626c4501814d2a79554ae406739b89da26c6
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2018-01-12 17:16:44 +0530

    the mobile sidebar is done 🎉

--- lumierevr2018/src/components/headerComponent/header.jsx
+++ lumierevr2018/src/components/headerComponent/header.jsx
@@ -1,35 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import "./header.css";
 
-export default () => {
-  return (
-    <nav>
-      <div className="container no-limit">
-        <div className="tabs-wrap">
-          <Link to="/" className="logo">
-            <img src={require("../../images/header/logo.png")} />
-          </Link>
+const renderNavPills = () => (
+  <ul>
+    <li>
+      <Link to="/"> Create </Link>
+    </li>
+    <li>
+      <Link to="/distribute"> Distribute </Link>
+    </li>
+    <li>
+      <Link to="/">Contacts</Link>
+    </li>
+    <li className="last">
+      <Link to="/careers">Join</Link>
+    </li>
+  </ul>
+);
 
-          <div className="nav-pills">
-            <ul>
-              <li className="first">
-                <Link to="/"> Create </Link>
-              </li>
-              <li>
-                <Link to="/distribute"> Distribute </Link>
-              </li>
-              <li>
-                <Link to="/">Contacts</Link>
-              </li>
-              <li className="last">
-                <Link to="/careers">Join</Link>
-              </li>
-            </ul>
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sidebarOpen: false
+    };
+  }
+
+  _toggleSidebar = () =>
+    this.setState({ sidebarOpen: !this.state.sidebarOpen });
+
+  render() {
+    return (
+      <nav>
+        <div className="container no-limit">
+          <div className="tabs-wrap">
+            <Link to="/" className="logo">
+              <img src={require("../../images/header/logo.png")} />
+            </Link>
+
+            <div className="nav-pills desktop">{renderNavPills()}</div>
+
+            <div
+            className={`sidebar-trigger ${this.state.sidebarOpen ? `opened` : ``}`}
+             onClick={this._toggleSidebar}>
+              <div className="bar" />
+              <div className="bar" />
+              <div className="bar" />
+            </div>
+
+            <div
+              className={`side-bar ${this.state.sidebarOpen ? `opened` : ``}`}
+            >
+              {renderNavPills()}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
