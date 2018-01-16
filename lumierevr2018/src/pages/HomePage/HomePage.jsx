30a5d5031b3dbf166ee00404a036062fa777a354
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2018-01-16 19:25:57 +0530

    Done with Homepage

--- lumierevr2018/src/pages/HomePage/HomePage.jsx
+++ lumierevr2018/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import moment from "moment";
+
+import CountdownClock from "../../components/CountdownClock/CountdownClock";
 
 import "./Homepage.css";
 
@@ -8,7 +11,7 @@ class Homepage extends Component {
       <div className="homepage page">
         <section className="splash-screen first-section">
           <video
-            poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg"
+            poster={require("../../images/HomePage/poster.png")}
             id="bgvid"
             playsInline
             autoPlay
@@ -32,19 +35,22 @@ class Homepage extends Component {
                 realize your brand's potential to engage your audience
                 emotionally.
               </p>
-              <button>Get Started !</button>
+              <button className="blue">Get Started</button>
             </div>
           </div>
         </section>
         <section className="victoria-section">
-          <div className="container">
-            <h2>Space for Victoria's Secret Models</h2>
+          <div className='timer-wrap'>
+            <h2>Coming Soon !</h2>
+            <CountdownClock endDate={moment("2018-02-14")} />
           </div>
         </section>
         <section className="vr-theatre-section">
           <img
-          className='background-img'
-          src={require("../../images/HomePage/theater.png")} alt="" />
+            className="background-img"
+            src={require("../../images/HomePage/theater.png")}
+            alt=""
+          />
           <div className="details">
             <div className="container">
               <h4>VR Theatre Solution</h4>
@@ -60,19 +66,56 @@ class Homepage extends Component {
                 See how creators and brands have used our systems to get
                 traction and even revenue !
               </p>
-              <button>Learn more!</button>
+              <button className="purple">Learn more</button>
               <img
-                className='retinad-logo'
+                className="retinad-logo"
                 src={require("../../images/HomePage/poweredbyRetinad.png")}
                 alt=""
               />
             </div>
           </div>
           <img
-              className="tablet-img"
-              src={require("../../images/HomePage/tablet.png")}
-              alt=""
-            />
+            className="tablet-img"
+            src={require("../../images/HomePage/tablet.png")}
+            alt=""
+          />
+        </section>
+        <section className="find-us-section">
+          <div className="container">
+            <h4>VR Theatre Solution</h4>
+            <ul className="address-grid">
+              <li>
+                <img
+                  src={require("../../images/HomePage/shenzhenMap.png")}
+                  alt=""
+                />
+                <h5>ShenZhen, China</h5>
+              </li>
+              <li>
+                <img src={require("../../images/HomePage/sfMap.png")} alt="" />
+                <h5>San Francisco, USA</h5>
+              </li>
+              <li>
+                <img
+                  src={require("../../images/HomePage/TorontoMap.png")}
+                  alt=""
+                />
+                <h5>Toronto, Canada</h5>
+              </li>
+            </ul>
+            <form>
+              <input type="text" placeholder="Name" />
+              <input type="email" placeholder="Email" />
+              <textarea
+                cols="30"
+                rows="10"
+                placeholder="How can we help you?"
+              />
+              <button type="submit" className="orange">
+                Send
+              </button>
+            </form>
+          </div>
         </section>
       </div>
     );
