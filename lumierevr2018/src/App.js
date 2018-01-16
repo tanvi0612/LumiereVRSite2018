90bb798acac7aa30dc1164d835d55d2ef16647f0
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2018-01-17 01:35:47 +0530

    migration

--- lumierevr2018/src/App.js
+++ lumierevr2018/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import "./assets/scss/default.css";
+import "./styles/default.css";
 
 // Global components
 import Header from "./components/headerComponent/header.jsx";
 import Footer from "./components/footerComponent/footer.jsx";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 // Pages
 import Homepage from "./pages/HomePage/HomePage.jsx";
@@ -14,18 +15,35 @@ import Distribute from "./pages/DistributePage/DistributePage";
 import CareerPage from "./pages/CareerPage/CareerPage";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      coverRevealed: false
+    };
+  }
+
   render() {
     return (
       <Router>
-        <div className="App">
-          <Header />
+        <div
+          className={`App ${!this.state.coverRevealed ? `scroll-lock` : ``}`}
+        >
+          <Header coverRevealed={this.state.coverRevealed} />
 
-          <div className="content-wrap">
-            <Route exact path="/create" component={Create} />
-            <Route exact path="/distribute" component={Distribute} />
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/careers" component={CareerPage} />
-          </div>
+          <LandingPage
+            coverRevealed={this.state.coverRevealed}
+            openLid={() => this.setState({ coverRevealed: true })}
+          />
+
+          {this.state.coverRevealed && (
+            <div className="content-wrap">
+              <Route exact path="/create" component={Create} />
+              <Route exact path="/distribute" component={Distribute} />
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/careers" component={CareerPage} />
+            </div>
+          )}
 
           <Footer />
         </div>
