7b9c256310e794dd5aa54ea974471965dc1f0de8
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2018-01-17 23:37:41 +0530

    the sidebar is working

--- lumierevr2018/src/components/headerComponent/header.jsx
+++ lumierevr2018/src/components/headerComponent/header.jsx
@@ -15,25 +15,30 @@ export default class Navbar extends Component {
   _toggleSidebar = () =>
     this.setState({ sidebarOpen: !this.state.sidebarOpen });
 
+  _onNavTabClick = () => {
+    if (!this.props.coverRevealed) this.props.openLid;
+    if (this.state.sidebarOpen) this._toggleSidebar();
+  };
+
   renderNavPills = props => (
     <ul>
       <li>
-        <NavLink onClick={this.props.openLid} to="/create">
+        <NavLink onClick={this._onNavTabClick} to="/create">
           Create
         </NavLink>
       </li>
       <li>
-        <NavLink onClick={this.props.openLid} to="/distribute">
+        <NavLink onClick={this._onNavTabClick} to="/distribute">
           Distribute
         </NavLink>
       </li>
       <li>
-        <NavLink onClick={this.props.openLid} to="/contracts">
+        <NavLink onClick={this._onNavTabClick} to="/contracts">
           Contacts
         </NavLink>
       </li>
       <li className="last">
-        <NavLink onClick={this.props.openLid} to="/careers">
+        <NavLink onClick={this._onNavTabClick} to="/careers">
           Join
         </NavLink>
       </li>
@@ -47,7 +52,7 @@ export default class Navbar extends Component {
           <div className="tabs-wrap">
             <Link
               to="/"
-              onClick={this.props.openLid}
+              onClick={this._onNavTabClick}
               className={`logo-wrap ${!this.props.coverRevealed
                 ? `outside-nav`
                 : ``}`}
