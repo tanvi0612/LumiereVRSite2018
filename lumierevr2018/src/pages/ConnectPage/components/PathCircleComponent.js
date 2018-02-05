a457a6949c9265f904e1c300801ab73570155c0e
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2018-02-06 02:12:21 +0530

    redirection from connect is working 👍

--- lumierevr2018/src/pages/ConnectPage/components/PathCircleComponent.js
+++ lumierevr2018/src/pages/ConnectPage/components/PathCircleComponent.js
@@ -5,22 +5,23 @@ export default props => {
     <div className="path-circle">
       <img src={props.backgroundImage} alt="" />
       <div className={`bottom-overlay ${props.active ? `selected` : ``}`}>
-        <h5 
-        onClick={() => props.selectTitle(props.title)}>{props.title}</h5>
-        
+        <h5 onClick={() => props.selectTitle(props.title)}>{props.title}</h5>
+
         {!!props.active && (
-          <form id="connect-form" onSubmit={props.handleSubmit}>
+          <form
+            id="connect-form"
+            onSubmit={e => props.handleSubmit(e, props.targetAfterSubmit)}
+          >
             <ul>
-          {props.secondaryTitles.map(title => (
-            <li
-              className={props.titleSelected === title ? `active` : ``}
-              
-              key={title}
-            >
-              {title}
-            </li>
-          ))}
-        </ul>
+              {props.secondaryTitles.map(title => (
+                <li
+                  className={props.titleSelected === title ? `active` : ``}
+                  key={title}
+                >
+                  {title}
+                </li>
+              ))}
+            </ul>
             <input type="email" name="email" placeholder="Email" />
             <button type="submit">Enter</button>
           </form>
