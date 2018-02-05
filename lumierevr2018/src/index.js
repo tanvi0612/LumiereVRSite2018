2d47e99350cd95fc71784dc457cce54c19dc65ca
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2018-02-06 01:59:12 +0530

    integrating GA

--- lumierevr2018/src/index.js
+++ lumierevr2018/src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import ReactGA from "react-ga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+// Integrating Google Analytics
+ReactGA.initialize("UA-113561379-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
